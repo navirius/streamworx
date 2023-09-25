450e4bd1b627d4bf60b13f691abae229c6455d5f
navirius <navirius@gmail.com>
2023-09-25 16:14:11 +0700

    fix gateway runable

--- apigateway/src/main/java/com/assignment/streamworx/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/assignment/streamworx/apigateway/ApigatewayApplication.java
@@ -2,8 +2,8 @@ package com.assignment.streamworx.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 
@@ -14,19 +14,19 @@ public class ApigatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayApplication.class, args);
     }
-
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("r1", r -> r.host("**.assignment.streamworx.com")
-                        .and()
-                        .path("/apigateway")
-                        .uri("http://assignment.streamworx.com"))
-                .route(r -> r.host("**.assignment.streamworx.com")
-                        .and()
-                        .path("/myOtherRouting")
-                        .filters(f -> f.prefixPath("/myPrefix"))
-                        .uri("http://othersite.com"))
-                        .build();
-    }
+//
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("r1", r -> r.host("**.assignment.streamworx.com")
+//                        .and()
+//                        .path("/apigateway")
+//                        .uri("http://assignment.streamworx.com"))
+//                .route(r -> r.host("**.assignment.streamworx.com")
+//                        .and()
+//                        .path("/myOtherRouting")
+//                        .filters(f -> f.prefixPath("/myPrefix"))
+//                        .uri("http://othersite.com"))
+//                        .build();
+//    }
 }
